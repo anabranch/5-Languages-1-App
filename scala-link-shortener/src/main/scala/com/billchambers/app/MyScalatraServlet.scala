1a052ad3501e48df78285c67e0ee5045e117ba57
anabranch <wac.chambers@gmail.com>
2015-06-02 09:25:46 -0700

    improved routing

--- scala-link-shortener/src/main/scala/com/billchambers/app/MyScalatraServlet.scala
+++ scala-link-shortener/src/main/scala/com/billchambers/app/MyScalatraServlet.scala
@@ -3,15 +3,26 @@ package com.billchambers.app
 import org.scalatra._
 import scalate.ScalateSupport
 
-class MyScalatraServlet extends MyScalaLinkShortenerStack {
+class Router extends MyScalaLinkShortenerStack {
 
   get("/") {
     <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+    <body>
+    <h1>Hello, world!</h1>
+    Say <a href="hello-scalate">hello to Scalate</a>.
       </body>
     </html>
   }
 
+  get("/:link") {
+    <html>
+    <body>
+    <h1>{params("link")}</h1>
+      </body>
+    </html>
+  }
+
+  post("/"){
+
+  }
 }
